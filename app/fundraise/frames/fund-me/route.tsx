7fdb16dde5734f51a9aa89bc252b30e82987bf84
0xDorcoPio <dioporco@tutamail.com>
2024-08-10 21:55:33 +0200

    wip: added blockscout endpoints

--- app/fundraise/frames/fund-me/route.tsx
+++ app/fundraise/frames/fund-me/route.tsx
@@ -4,6 +4,31 @@ import { Button } from "frames.js/next";
 
 const frameHandler = frames(async (ctx) => {
 
+  // get methods from https://optimism-sepolia.blockscout.com/api/v2/smart-contracts/0x506D428E0414478dadC772891028282831085331/methods-read?is_custom_abi=false&from=0x7A8E79dE63c29c3ee2375Cd3D2e90FEaA5aAf322
+
+  // read funds from: https://optimism-sepolia.blockscout.com/api/v2/smart-contracts/0x506D428E0414478dadC772891028282831085331/query-read-method
+  /*
+  {
+    "args": [
+    ],
+    "method_id": "968ed600",
+    "contract_type": "regular"
+  }
+  */
+
+  const blockscoutEndpoint = 'https://optimism-sepolia.blockscout.com/api/v2/smart-contracts/0x506D428E0414478dadC772891028282831085331'
+  try {
+    const response = await fetch(blockscoutEndpoint);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error : any) {
+    console.error(error.message);
+  }
+
   return {
     image: <div tw="flex">Donate to the campaign X/Y fund raised</div>, // TODO: put the amount
     textInput: "Enter amount",

5937de1e01cf9ad39bd4bf5bffabdc448fe559cf
0xDorcoPio <dioporco@tutamail.com>
2024-08-10 19:17:57 +0200

    feat: fixed thank you page

--- app/fundraise/frames/thank-you/route.tsx
+++ app/fundraise/frames/thank-you/route.tsx
@@ -2,14 +2,17 @@
 import { frames } from "./frames";
 import { Button } from "frames.js/next";
 
-export const POST = frames(async (ctx) => {
+const frameHandler = frames(async (ctx) => {
 
     return {
         image: <div tw="flex">Thank you for donating! </div>,
         buttons: [
-            <Button action="post" target="/">
+            <Button action="post" target="../">
                 Go back
             </Button>,
         ],
     };
 });
+
+export const GET = frameHandler;
+export const POST = frameHandler;
